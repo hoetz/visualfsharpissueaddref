1a088f4f88a347ee1fe62f08477b3b20e6d08a7a
Florian Hoetzinger <Florian.Hoetzinger@gab-net.com>
2017-12-30 10:18:16 +0100

    init

---
+++ ClassLibraryCS/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibraryCS
+{
+    public class Class1
+    {
+    }
+}
---
+++ ClassLibraryFS/Library.fs
@@ -0,0 +1,5 @@
+namespace ClassLibraryFS
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
